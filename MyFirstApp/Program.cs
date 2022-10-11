d6ec31c3e014fcf5929b80479d96991a23b23dfc
SKY_Win <3210654789@naver.com>
2022-10-12 01:21:06 +0900

    Delegate Study. Csharp Bulletin ING!

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,17 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
 
-namespace MyFirstApp
+namespace JustForTest
 {
-    internal class Program
+    //Class안에 선언해도 되고 이렇게 밖에 선언해도 됨
+    //delegate int Compare(int a, int b);
+    class MainApp
     {
+        delegate int Compare(int a, int b);
+
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else if (a == b)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+            {
+                return -1;
+            }
+            else if (a == b)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void BubbleSort(int[] dataSet, Compare comparer)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+            for (i = 0; i < dataSet.Length-1; i++)
+            {
+                for (j = 0; j < dataSet.Length-i-1; j++)
+                {
+                    if (comparer(dataSet[j], dataSet[j+1]) > 0)
+                    {
+                        temp = dataSet[j + 1];
+                        dataSet[j + 1] = dataSet[j];
+                        dataSet[j] = temp;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey(); 
+            int[] array = { 3, 7, 4, 2, 10 };
+            Console.WriteLine("Sorting Ascending...");
+
+            BubbleSort(array, new Compare(AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
         }
     }
 }
--- RoadBook.CsharpBasic.Chapter10/Examples/Ex001.cs
+++ RoadBook.CsharpBasic.Chapter10/Examples/Ex001.cs
@@ -4,6 +4,7 @@ using System.Text;
 using RoadBook.CsharpBasic.Chapter10.Models;
 using RoadBook.CsharpBasic.Chapter10.Manager;
 using System.Linq;
+using Microsoft.SqlServer.Server;
 
 namespace RoadBook.CsharpBasic.Chapter10.Examples
 {
@@ -11,6 +12,7 @@ namespace RoadBook.CsharpBasic.Chapter10.Examples
     {
         public void Run()
         {
+
             DatabaseInfo dbInfo = new DatabaseInfo();
             dbInfo.Name = "RoadbookPractice";
             dbInfo.Ip = "127.0.0.1";
@@ -39,7 +41,7 @@ namespace RoadBook.CsharpBasic.Chapter10.Examples
                 {
                     ms.Close();
 
-                    Console.WriteLine("BYT");
+                    Console.WriteLine("BYE");
                     break;
                 }
                 else
@@ -56,13 +58,43 @@ namespace RoadBook.CsharpBasic.Chapter10.Examples
                     switch (input)
                     {
                         case "1"://SELECT
-                            DataTable dt = ms.Select("")
+                            DataTable dt = ms.Select("SELECT IDX, TITLE, SUMMARY, CREATE_DT, CREATE,USER,NM, TAGS, LIKE_CNT, CATEGORY_IDX, FROM TB_CONTENTS");
+                            if (dt.Rows.Count > 0)
+                            {
+                                string[] columns = new string[dt.Columns.Count];
+                                for (int idx = 0; idx < dt.Columns.Count; idx++)
+                                {
+                                    columns[idx] = dt.Columns[idx].ToString();
+
+                                    Console.WriteLine(dt.Columns[idx] + "\t");
+                                }
+
+                                Console.WriteLine();
+
+                                for (int idx = 0; idx < dt.Rows.Count; idx++)
+                                {
+                                    for (int idx_j = 0; idx_j < dt.Columns.Count; idx_j++)
+                                    {
+                                        Console.WriteLine(dt.Rows[idx][columns[idx_j]] + "\t");
+                                    }
+
+                                    Console.WriteLine();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No Data!!");
+                            }
+
+                            break;
+                        case "2":
+
                         default:
                             break;
                     }
                 }
             }
-           
+
         }
     }
 }
